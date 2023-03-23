4c20c1a544d8e271699482631f7386e7880f96fa
Jesper Kling <jesperkling87@gmail.com>
2023-03-23 17:56:36 +0100

    Added pages and can now navigate between them using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+// style
 import "./App.css";
+
+// React Router
+import { Route, Routes } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import Map from "./pages/Map";
+import Restaurants from "./pages/Restaurants";
+import Restaurant from "./pages/Restaurant";
+import Tips from "./pages/Tips";
 
 // Components
 import Header from "./components/Header";
@@ -8,6 +19,13 @@ function App() {
     <div className="App">
       <Header />
       <h1>Hungry App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/restaurants/:id" element={<Restaurant />} />
+        <Route path="/tips" element={<Tips />} />
+      </Routes>
     </div>
   );
 }
