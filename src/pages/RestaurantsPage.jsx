f8ebd45f308a96ea1a115659617df1f1ba5f0fd4
Jesper Kling <jesperkling87@gmail.com>
2023-04-01 01:30:02 +0200

    Added logic to restaurants pages

--- src/pages/RestaurantsPage.jsx
+++ src/pages/RestaurantsPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Container, ListGroup } from "react-bootstrap";
 
 function RestaurantsPage() {
-  return <div>Restaurants</div>;
+  const restaurants = [
+    { id: 1, name: "Restaurant A" },
+    { id: 2, name: "Restaurant B" },
+    { id: 3, name: "Restaurant C" },
+  ];
+
+  return (
+    <Container>
+      <h1>Restaurants</h1>
+      <ListGroup>
+        {restaurants.map((restaurant) => (
+          <ListGroup.Item key={restaurant.id}>
+            <Link
+              to={{
+                pathname: `/restaurants/${restaurant.id}`,
+                search: `name=${restaurant.name}`,
+              }}
+            >
+              {restaurant.name}
+            </Link>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
+  );
 }
 
 export default RestaurantsPage;
