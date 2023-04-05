22223246d1e29255dd382e32f206d0d7639184b8
Jesper Kling <jesperkling87@gmail.com>
2023-04-05 23:35:59 +0200

    Added Google Maps with marker to MapPage

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 function MapPage() {
-  return <div></div>;
+  const containerStyle = {
+    width: "100%",
+    height: "600px",
+  };
+
+  const center = {
+    lat: 55.605,
+    lng: 13.003,
+  };
+
+  const [map, setMap] = useState(null);
+  const [isMapReady, setIsMapReady] = useState(false);
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+  });
+
+  const onLoad = useCallback(async function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+
+    // Wait for 500 milliseconds before setting the map
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    setMap(map);
+    setIsMapReady(true);
+    map.setZoom(12);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return (
+    <>
+      {!isLoaded && !loadError && <div>Loading...</div>}
+      {loadError && <div>Map cannot be loaded right now, sorry.</div>}
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={12}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          {/* Add a marker */}
+          {isMapReady && (
+            <Marker
+              position={{ lat: 55.606158395718786, lng: 13.024105474863351 }}
+              onClick={() => {
+                map.setZoom(16);
+                map.panTo({ lat: 55.606158395718786, lng: 13.024105474863351 });
+              }}
+            />
+          )}
+        </GoogleMap>
+      )}
+    </>
+  );
 }
 
 export default MapPage;
