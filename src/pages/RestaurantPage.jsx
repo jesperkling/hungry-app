1f3a603a8129ddd3bb0f8276da413af085a18861
Jesper Kling <jesperkling@outlook.com>
2023-04-29 23:14:37 +0200

    Fixed place page design

--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,29 +1,93 @@
 import { useParams } from "react-router-dom";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Row } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 import useGetPlace from "../hooks/useGetPlace";
 
 function RestaurantPage() {
   const { id } = useParams();
   const { data: place, isLoading } = useGetPlace(id);
-  console.log(place);
 
   if (!place) {
     return <Container>Loading...</Container>;
   }
 
   return (
-    <Container>
+    <Container className="mt-4">
       {isLoading && <p>Laddar...</p>}
-      <Card>
-        <Card.Body>
-          <Card.Title>{place.namn}</Card.Title>
-          <Card.Subtitle>
-            {place.gatuadress} {place.gatunummer}
-          </Card.Subtitle>
-          <Card.Text>+46{place.telefon}</Card.Text>
-          <Card.Link href={place.hemsida}>Hemsida</Card.Link>
-        </Card.Body>
-      </Card>
+      {place && (
+        <Card>
+          <Card.Body>
+            <Card.Title>{place.namn}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              Adress: {place.gatuadress} {place.gatunummer}
+            </Card.Subtitle>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                Telefon:{" "}
+                <a href={`tel: ${place.telefon}`}>+46{place.telefon}</a>
+              </ListGroup.Item>
+              <ListGroup.Item>Beskrivning: {place.beskrivning}</ListGroup.Item>
+              <ListGroup.Item>Cuisine: {place.cuisine}</ListGroup.Item>
+              <ListGroup.Item>Typ: {place.typ}</ListGroup.Item>
+              <ListGroup.Item>Utbud: {place.utbud}</ListGroup.Item>
+            </ListGroup>
+            <div className="m-2 text-center">
+              {place.hemsida && (
+                <Card.Link
+                  href={place.hemsida}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="../src/assets/images/website.svg"
+                    alt="website"
+                    style={{ width: "32px", height: "32px" }}
+                  />
+                </Card.Link>
+              )}
+              {place.facebook && (
+                <Card.Link
+                  href={place.facebook}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="../src/assets/images/fb-logo.svg"
+                    alt="facebook"
+                    style={{ width: "32px", height: "32px" }}
+                  />
+                </Card.Link>
+              )}
+              {place.instagram && (
+                <Card.Link
+                  href={place.instagram}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="../src/assets/images/instagram-logo.svg"
+                    alt="instagram"
+                    style={{ width: "32px", height: "32px" }}
+                  />
+                </Card.Link>
+              )}
+              {place.epost && (
+                <Card.Link
+                  href={`mailto:${place.epost}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="../src/assets/images/email.svg"
+                    alt="email"
+                    style={{ width: "32px", height: "32px" }}
+                  />
+                </Card.Link>
+              )}
+            </div>
+          </Card.Body>
+        </Card>
+      )}
     </Container>
   );
 }
