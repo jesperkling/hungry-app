377af9ee982eb016e220b97f8624fa8c84504aab
Jesper Kling <jesperkling@outlook.com>
2023-04-30 01:27:20 +0200

    Moved images to public folder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import backgroundImage from "/images/bild-4.jpg";
+import backgroundImage from "/bild-4.jpg";
 
 function HomePage() {
   return (
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -2,10 +2,10 @@ import { useParams } from "react-router-dom";
 import { Container, Card, Row } from "react-bootstrap";
 import { ListGroup } from "react-bootstrap";
 import useGetPlace from "../hooks/useGetPlace";
-import facebook from "/images/fb-logo.svg";
-import instagram from "/images/instagram-logo.svg";
-import hemsida from "/images/website.svg";
-import email from "/images/email.svg";
+import facebook from "/fb-logo.svg";
+import instagram from "/instagram-logo.svg";
+import hemsida from "/website.svg";
+import email from "/email.svg";
 
 function RestaurantPage() {
   const { id } = useParams();
